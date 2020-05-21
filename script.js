e606217c4084acc1375d3f28e7a8332967a00735
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-05-21 16:40:54 -0300

    commit first requirerment

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
-window.onload = function onload() { };
-
+async function doFetch(QUERY) {
+  const resp = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+  const data = await resp.json();
+  const doMap = data.results.map((obj) => {
+    const filter = { sku: obj.id,
+      name: obj.title,
+      image: obj.thumbnail,
+      salePrice: obj.price,
+    };
+    return filter;
+  });
+  return doMap;
+}
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
-
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+
+window.onload = async function onload() {
+  const items = document.querySelector('.items');
+  const sync = await doFetch('computador');
+  sync.map(objeto => items.appendChild(createProductItemElement(objeto)));
+};
