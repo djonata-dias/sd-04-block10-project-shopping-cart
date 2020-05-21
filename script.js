dd5f75ace4a66e9aa4de626794ee72bf2a9815e7
deboracosilveira <deboracosilveira@gmail.com>
2020-05-21 19:05:04 -0300

    Requirement 1 - Cypress again

--- script.js
+++ script.js
@@ -28,22 +28,19 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   const button = document.querySelectorAll('.item__add');
-//   // console.log(button)
+function cartItemClickListener(event) {
+  const button = document.querySelectorAll('.item__add');
+  // console.log(button)
 
-//   button[0].addEventListener('click', (event) => {
-//     const clickedItem = event.target.parentNode;
-//     const clickedItemSku = clickedItem.firstChild.innerText;
+  button[0].addEventListener(event, (e) => {
+    const clickedItem = e.target.parentNode;
+    const clickedItemSku = clickedItem.firstChild.innerText;
 
-//     fetch(`https://api.mercadolibre.com/items/${clickedItemSku}`)
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-
-//   })
-
-//   createCartItemElement(product)
-// }
+    fetch(`https://api.mercadolibre.com/items/${clickedItemSku}`)
+    .then(response => response.json())
+    .then(json => console.log(json));
+  });
+}
 
 // function createCartItemElement({ sku, name, salePrice }) {
 //   const li = document.createElement('li');
@@ -66,20 +63,7 @@ const productInfo = (productArray) => {
 
     items[0].appendChild(createProductItemElement(item));
 
-    // const button = document.querySelectorAll('.item__add');
-    // // console.log(button)
-
-    // button[0].addEventListener('click', (event) => {
-    //   const clickedItem = event.target.parentNode;
-    //   const clickedItemSku = clickedItem.firstChild.innerText;
-
-    //   fetch(`https://api.mercadolibre.com/items/${clickedItemSku}`)
-    //   .then(response => response.json())
-    //   .then(json => console.log(json))
-
-    // })
-
-    // cartItemClickListener(event);
+    cartItemClickListener('click');
   });
 };
 
