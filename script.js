ac655015f326af2b0f524837ba52f47b5862d3bb
juniomelos <you@example.com>
2020-05-21 18:48:23 -0300

    starting the project

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-window.onload = function onload() { };
+const API_URL = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+const myObj = { method: "GET" };
+
+const creatList = (dados) => {
+  const arrayList = [];
+  dados.results.forEach((element) => {
+    arrayList.push(element);
+  });
+  //  console.log(arrayList)
+  const arrayProducts = [];
+  arrayList.forEach((e) => {
+    arrayProducts.push({
+      ski: e.id,
+      name: e.title,
+      image: e.thumbnail,
+    });
+    console.log(arrayProducts);
+  });
+  return arrayProducts;
+};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -15,19 +34,19 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement("button", "item__add", "Adicionar ao carrinho!"));
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
@@ -35,9 +54,16 @@ function cartItemClickListener(event) {
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
+  const li = document.createElement("li");
+  li.className = "cart__item";
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener("click", cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  fetch(API_URL, myObj)
+    .then((data) => data.json())
+    .then((dados) => creatList(dados))
+    .catch((error) => console.log(error));
+};
