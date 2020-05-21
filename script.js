9031eed291b080ddd195b9379106cce928a81338
danilossousa <danilosantossousa@gmail.com>
2020-05-21 16:37:20 -0300

    Init

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() {const toot = 0;};
+window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
