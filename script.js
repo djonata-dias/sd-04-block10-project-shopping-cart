bd40d13fc248963166c96f96ddde9f126d81d32c
deboracosilveira <deboracosilveira@gmail.com>
2020-05-21 17:06:09 -0300

    Requirement 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,18 +24,48 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const productInfo = (productArray) => {
+  const items = document.getElementsByClassName('items');
+  // const productList = [];
+
+  productArray.forEach((product) => {
+    const { id, title, thumbnail } = product;
+    const item = {
+      sku: id,
+      name: title,
+      image: thumbnail,
+    };
+
+    items[0].appendChild(createProductItemElement(item));
+    // productList.push(item);
+  });
+  // console.log(productList)
+  // return productList;
+};
+
+window.onload = function onload() { };
+
+
+const $QUERY = 'computador';
+
+fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`)
+  .then(response => response.json())
+  .then(json => productInfo(json.results));
+
+  // .catch(err => console.log("err"))
