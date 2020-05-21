f3d37cb40309fa3e9627b7a401be90112a786a98
Eduardo Santos <eduardo.debest81@gmail.com>
2020-05-21 15:24:54 -0300

    1. Listagem de produtos

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-window.onload = function onload() { };
+window.onload = () => {
+  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+    .then((response) => response.json())
+    .then((r) => r.results.forEach((elem) => {
+      console.log(elem);
+      document.querySelector('.items').appendChild(createProductItemElement(elem));
+    }));
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,13 +21,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
