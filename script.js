af017facdae8493b70ca174def522e1df1175978
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-05-21 20:47:21 -0300

    test-ci

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -21,7 +19,11 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  const btn = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  btn.addEventListener('click', () => addIntoCart(sku));
+  section.appendChild(btn);
 
   return section;
 }
@@ -34,14 +36,43 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
+async function getDetailsToCart(id) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const respToJson = await response.json();
+  const sku = respToJson.id;
+  const name = respToJson.title;
+  const salePrice = respToJson.price;
+  return { sku, name, salePrice };
+}
+
+const addIntoCart = async (id) => {
+  const getDetails = await getDetailsToCart(id);
+  createCartItemElement(getDetails);
+};
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  return li;
+  document.querySelector('.cart__items').appendChild(li);
+}
+
+function tratarRetornoDaApi(dados) {
+  dados.forEach((e) => {
+    const sku = e.id;
+    const name = e.title;
+    const image = e.thumbnail;
+    document.querySelector('.items').appendChild(createProductItemElement({ sku, name, image }));
+  });
 }
 
 
+window.onload = function onload() {
+  let query = 'computador'// parametro para busca na API
 
-
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((batata) => batata.json()) // Fica aguardando a nossa requisicao ser completada com sucesso, enquanto está sendo executada de forma assincrona.
+  .then((dadosEmFormatoJson) => tratarRetornoDaApi(dadosEmFormatoJson.results))
+  .catch((error) => console.log(error))
+};
