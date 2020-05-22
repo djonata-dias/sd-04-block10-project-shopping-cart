b08feaab4e9e971a121559cb5b455256df0eef28
juniomelos <you@example.com>
2020-05-22 17:11:28 -0300

    Requirement 3 - try 5 cc

--- script.js
+++ script.js
@@ -88,5 +88,6 @@ const createList = async () => {
 window.onload = async function onload() {
   await createList();
   document.getElementsByClassName('cart__items')[0].innerHTML = '';
-  //document.querySelectorAll('li').forEach(li => li.addEventListener('click', cartItemClickListener))
+  //  document.querySelectorAll('li').forEach(li =>
+  //  li.addEventListener('click', cartItemClickListener))
 };
