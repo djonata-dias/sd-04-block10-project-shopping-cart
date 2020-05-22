744847ec2a217299c76dd2c8260e7ec7e2ce75a7
frederico-campello <fredfmc33@gmail.com>
2020-05-22 09:31:57 -0300

    Primeiro requisito

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,8 +13,8 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement('section'); // cria uma section
+  section.className = 'item'; // atribui à section criada a classe .item
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
@@ -26,18 +24,51 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li; // elementoOL.appendChild(createCartItemElement({ sku, name, salePrice }))
+// }
+
+// -------------------------------------------------------------------- //
+
+// requisito 1 -> retorno da requisicao feita no onload é usado para listar os produtos
+
+const produtos = (produto) => {
+  // console.log(produto) -> array de objetos (cada objeto com infos de um produto)
+
+  produto.forEach((elemento) => {
+    const sku = elemento.id;
+    const name = elemento.title;
+    const image = elemento.thumbnail;
+    const itemsElement = document.querySelector('.items');
+    itemsElement.appendChild(createProductItemElement({ sku, name, image }));
+  });
+};
+
+
+// requisito 2 -> botao adicionar ao carrinho dispara uma requisicao a api
+
+// const botaoAdicionar = document.querySelectorAll('.item__add');
+// console.log(botaoAdicionar); // Node List
+
+
+// onload -> toda vez que a pagina é carregada é feita uma requisicao na API do mercado livre
+
+window.onload = function onload() {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(API_URL)
+  .then(response => response.json()) // sucesso na requisicao, retorna um objeto json (uma promise)
+  .then(produto => produtos(produto.results))
+  .catch(error => console.log(error));
+};
