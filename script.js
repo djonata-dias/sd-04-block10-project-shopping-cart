56ea144f7eaf0ba22af6182b74843b88880a6951
juniomelos <you@example.com>
2020-05-22 10:05:24 -0300

    Requisito 2 - test eslint

--- script.js
+++ script.js
@@ -23,7 +23,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 }
@@ -66,9 +68,10 @@ const createList = (dados) => {
   return arrayProducts;
 };
 
-const printList = array => array.forEach((e) => {
-  sectionItems[0].appendChild(createProductItemElement(e));
-});
+const printList = array =>
+  array.forEach((e) => {
+    sectionItems[0].appendChild(createProductItemElement(e));
+  });
 
 //  Requirement 2
 
