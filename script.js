5b7a2d4eece6ad980c0b424c4503c46e1452fc4e
juniomelos <you@example.com>
2020-05-22 15:31:22 -0300

    Requirement 2 - cc issues 2

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -58,10 +62,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 //  Function here...
