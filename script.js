e0987be8a07b8ccf4d34f0ea9a41ff1087280401
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-05-22 18:31:16 -0300

    test request 7 comment remove span

--- script.js
+++ script.js
@@ -124,9 +124,9 @@ window.onload = function onload() {
           createProductItemElement({
             sku: result.id, name: result.title, image: result.thumbnail }));
       });
-      setTimeout(function () {
-        span.parentNode.removeChild(span);
-      }, 1000);
+      // setTimeout(function () {
+      //   span.parentNode.removeChild(span);
+      // }, 1000);
     })
     .catch(error => console.log(error));
   document.body.addEventListener('click', function (event) {  // find the Id of the clicked add to cart button
