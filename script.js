c8907b350455c1e1a30a884d1f936181ed99224a
joaosoutto <joaosoutto92@gmail.com>
2020-05-22 19:00:37 -0300

    DEUS E MAAAAAAAAAAAAIS

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function createProductImageElement(imageSource) { // FUNÇÃO PADRÃO DO PROJETO
   return img;
 }
 
-function createCustomElement(element, className, innerText) { // FUNÇÃO PADRÃO DO PROJETO
+  // FUNÇÃO PADRÃO DO PROJETO -> criar objeto customizado,
+function createCustomElement(element, className, innerText) {
+  // recebendo como parâmetro a tag, classe e texto.
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
@@ -13,7 +15,8 @@ function createCustomElement(element, className, innerText) { // FUNÇÃO PADRÃ
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) { // FUNÇÃO PADRÃO DO PROJETO
+  // FUNÇÃO PADRÃO DO PROJETO
+function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -29,10 +32,12 @@ function createProductItemElement({ sku, name, image }) { // FUNÇÃO PADRÃO DO
 // function getSkuFromProductItem(item) {   // FUNÇÃO PADRÃO DO PROJETO
 //   return item.querySelector('span.item__sku').innerText;
 // }
+//  -------------------------------------------------------------------------------------
 
 function cartItemClickListener(event) { // FUNÇÃO PADRÃO DO PROJETO
   event.target.remove(); // ADICIONANDO FUNÇÃO DE REMOVER O ELEMENTO AO CLICAR SOBRE ELE
 }
+//  -------------------------------------------------------------------------------------
 
 function createCartItemElement({ sku, name, salePrice }) {  // FUNÇÃO PADRÃO DO PROJETO
   const li = document.createElement('li');
@@ -40,9 +45,11 @@ function createCartItemElement({ sku, name, salePrice }) {  // FUNÇÃO PADRÃO 
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   document.querySelectorAll('.cart__items')[0].appendChild(li);
+  // SALVANDO OS ITENS DO CARRINHO DENTRO DO LOCAL STORAGE CRIADO AQUI
   localStorage.fullCart = document.querySelector('ol.cart__items').innerHTML;
   return li;
 }
+//  -------------------------------------------------------------------------------------
 
 const productInfo = (event) => {  // FUNÇÃO PARA PEGAR AS INFORMAÇÕES DE ITME POR ITEM
   const itemID = event.target.parentNode.firstChild.innerText;
@@ -51,6 +58,7 @@ const productInfo = (event) => {  // FUNÇÃO PARA PEGAR AS INFORMAÇÕES DE ITM
     .then(data => createCartItemElement({ sku: data.id, name: data.title, salePrice: data.price }))
     .catch(console.error);
 };
+//  -------------------------------------------------------------------------------------
 
 const returnProduct = (results) => {  //  FUNÇÃO PARA LISTAR OS PRODUTOS NA TELA
   const product = {};
@@ -66,6 +74,7 @@ const returnProduct = (results) => {  //  FUNÇÃO PARA LISTAR OS PRODUTOS NA TE
         item.addEventListener('click', event => productInfo(event));
       });
 };
+//  -------------------------------------------------------------------------------------
 
 const fetchAPI = (URL) => { // FUNÇÃO FETCH PRA SER CHAMADA NO ONLOAD
   fetch(URL)
@@ -73,20 +82,33 @@ const fetchAPI = (URL) => { // FUNÇÃO FETCH PRA SER CHAMADA NO ONLOAD
   .then(data => returnProduct(data.results))
   .catch(console.error);
 };
+//  -------------------------------------------------------------------------------------
 
-const loading = () =>
+const loading = () => // CRIANDO O ELEMENTO (SPAN) DO LOADING DENTRO DO HEADER COM CLASSE 'TOP-BAR'
   document
-    .getElementsByClassName('top-bar')[0]
-    .appendChild(createCustomElement('span', 'loading', 'loading...'));
+    .getElementsByClassName('top-bar')[0].appendChild(createCustomElement('span', 'loading', 'loading...'));
 
-window.onload = function onload() {
-  fetchAPI('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  loading();
-  setTimeout(() => document.querySelector('.loading').remove(), 500);
+
+    //----------------------------------------------------------------------------------
+
+const eraseAll = () => {  // SALVANDO OS ARQUIVOS NO LOCAL STORAGE
   if (Storage) {
     document.querySelector('ol.cart__items').innerHTML = localStorage.fullCart;
   }
   document.querySelector('button.empty-cart').addEventListener('click', () => {
-    document.querySelector('ol.cart__items').innerHTML = '';
+    document.querySelector('ol.cart__items').innerHTML = ''; // ADICIONANDO A FUNÇÃO DE EXPCLUIR TODOS OS
+    localStorage.fullCart = '';                              // ITENS DO CARRINHO DE COMPRAS
   });
+};
+//  -------------------------------------------------------------------------------------
+
+window.onload = function onload() {
+  // CHAMANDO A FUNÇÃO DO FETCH 1
+  fetchAPI('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  // CHAMANDO O OBJETO 'LOADING'
+  loading();
+  //  COLOCANDO O SETTIMEOUT PRO LOADING SUMIR
+  setTimeout(() => document.querySelector('.loading').remove(), 2000);
+  // CHAMANDO A FUNÇÃO DE APAGAR OS ITENS DO CARRINHO
+  eraseAll();
 };
