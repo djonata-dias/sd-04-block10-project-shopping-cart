062d0cbea9a425ceea3c4a859a3b8b86d1e79371
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-22 17:38:40 -0300

    Iniciando o projeto. Vamos com tudo!

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 window.onload = function onload() { };
 
+/*
+  1. Listagem de produtos
+  Você deve criar uma listagem de produtos que devem 
+    ser consultados através da API do Mercado Livre.
+  Você deve utilizar o endpoint:
+    "https://api.mercadolibre.com/sites/MLB/search?q=$QUERY"
+
+  onde $QUERY deve ser o valor da sua busca. 
+    Para este trabalho, a busca deve ser o termo computador.
+  O retorno desse endpoint será algo no formato json. 
+    Por exemplo, se for pesquisado "computador": veja no Json no readme.
+
+  A lista de produtos que devem ser exibidos é o array results no JSON acima.
+
+  Você deve utilizar a função createProductItemElement(product) 
+    para criar os componentes HTML referentes a um produto.
+
+  Adicione o elemento retornado da função createProductItemElement(product) 
+    como filho do elemento <section class="items">.
+
+  Obs: as variáveis sku, no código fornecido, 
+    se referem aos campos id retornados pela API. 
+*/
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
