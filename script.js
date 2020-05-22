8dcf716712cc76305a960311a0def6fc0886be8d
AliceAAtalla <aliceatalla36@gmail.com>
2020-05-22 15:25:47 -0300

    Requisito 1 - Updates and CC2

--- script.js
+++ script.js
@@ -26,28 +26,12 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-// coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
+// -----------------------------------------------------
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
 const search = value => window.fetch(`${API_URL}${value}`);
 
 const extraiPesquisa = data => data.json();
-
-const sectionNaSection = (section) => {
-  const containerItem = document.querySelector('.items');
-  containerItem.appendChild(section);
-};
 
 const buscarNoObj = ({ id, title, price, thumbnail }, fun) => {
   const newObjeto = {
@@ -60,17 +44,49 @@ const buscarNoObj = ({ id, title, price, thumbnail }, fun) => {
 };
 
 const requisito1 = async () => {
+  const containerItem = document.querySelector('.items');
   try {
     const searchComputer = await search('computador');
     const json = await extraiPesquisa(searchComputer);
-    const resultado = await json.results;
-    const resultObj = await buscarNoObj(resultado[0], createProductItemElement);
-    await sectionNaSection(resultObj);
+    await json.results.forEach((elementos) => {
+      containerItem.appendChild(buscarNoObj(elementos, createProductItemElement));
+    });
   } catch (error) {
-    console.log('Ixi, deu erro: ', error);
+    console.log('Ixi, deu erro no requisito 1: ', error);
   }
 };
 
+// ----------------------------------------------------
+// function cartItemClickListener(event) {
+//   const ol = document.querySelector('.cart__items');
+//   const item = document.querySelector('.cart__item');
+//   ol.appendChild(item);
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+// const API_URL_CART = 'https://api.mercadolibre.com/items/';
+
+// const cartItem = itemID => window.fetch(`${API_URL_CART}${itemID}`);
+
+// const requisito2 = async () => {
+//   try {
+//     const search = await cartItem('MLB1341706310');
+//     const json = await extraiPesquisa(search);
+//     const creatElement = await buscarNoObj(json, createCartItemElement);
+//     console.log(json);
+//   } catch (error) {
+//     console.log('Ixi, deu erro no requisito 2: ', error);
+//   }
+// };
+
 window.onload = () => {
   requisito1();
+  // requisito2();
 };
