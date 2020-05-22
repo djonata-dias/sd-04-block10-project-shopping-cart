8d02ed558ddb6dd1189b2615dbc09c8df39558b2
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-05-22 15:42:45 -0300

    stting local storage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 /* eslint-disable arrow-parens */
 const mainSection = document.querySelector('.items');
 const cartSection = document.querySelector('.cart__items');
+console.log(cartSection);
 
 const productInfo = (products) =>
   products.map(({ id, title, thumbnail, price }) => {
@@ -42,19 +43,43 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// removing itens from the cart by clicking on them:
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
 // creating elements for each item in the cart section:
 function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// removing itens from the cart by clicking on them:
+let arrLStorage = [];
+function cartItemClickListener(event) {
+  // removing element only if it's a list
+  if (event.target && event.target.nodeName === 'LI') event.target.remove();
+  // removing the product from the storage array
+  arrLStorage.splice(event.target.innerHTML, 1);
+  // than setting the new array as the current storage
+  localStorage.setItem('items', JSON.stringify(arrLStorage));
+}
+cartSection.addEventListener('click', cartItemClickListener);
+
+// adding to localStorage, this function is called each time we do a fetch:
+const addingToStorage = (product) => {
+  arrLStorage = JSON.parse(localStorage.getItem('items'));
+  arrLStorage.push(product.innerHTML);
+  localStorage.setItem('items', JSON.stringify(arrLStorage));
+};
+
+// loading localStorage when the page loads:
+const loadingLS = () => {
+  const dataFromLStorage = JSON.parse(localStorage.getItem('items'));
+  dataFromLStorage.forEach(elem => {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerHTML = elem;
+    cartSection.appendChild(li);
+  });
+};
 
 // adding items to cart by clicking their buttons:
 mainSection.addEventListener('click', function (event) {
@@ -62,17 +87,26 @@ mainSection.addEventListener('click', function (event) {
     const itemID = event.target.parentNode.firstChild.innerText;
     fetch(`https://api.mercadolibre.com/items/${itemID}`)
       .then((data) => data.json())
-      .then((products) => cartSection.appendChild(createCartItemElement(products)));
+      .then((products) => cartSection.appendChild(createCartItemElement(products)))
+      .then(li => addingToStorage(li));
   }
 });
 
+// loading local storage, only if it's not empty:
+if (localStorage.getItem('items') != null) {
+  loadingLS();
+}
+
+// fetching products informations to the main section:
 window.onload = function onload() {
   const queryInput = document.querySelector('.query-input').value;
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${queryInput}`)
     .then((data) => data.json())
     .then((json) => json.results)
     .then((products) => productInfo(products)
-      .forEach((product) => mainSection.appendChild(createProductItemElement(product))));
+      .forEach((product) => {
+        mainSection.appendChild(createProductItemElement(product));
+      }));
 };
 
 // function getSkuFromProductItem(item) {
