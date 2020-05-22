ef108c8660b80fed41f44a9e80e8fb0fc29983f0
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-22 10:13:47 -0300

    Exercise 1 - agora vai

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  // é aqui que se cria o botão.,
   return e;
 }
 
@@ -19,7 +20,6 @@ function createProductItemElement({ sku, name, image }) {
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
-  document.getElementsByClassName('items')[0].appendChild(section);
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
@@ -44,21 +44,19 @@ function createProductItemElement({ sku, name, image }) {
 
 // criando a chamada da função que busca o produto.
 
-// const returnProduct = (results) => {
-//   const product = {};
-//   results.forEach((item) => {
-//     product.sku = item.id;
-//     product.name = item.title;
-//     product.image = item.thumbnail;
-//     const section = createProductItemElement(product);
-//     document.getElementsByClassName('items')[0].appendChild(section);
-//   });
-// };
-
+const returnProduct = (results) => {
+  const product = {}; // inicializar a contagem ou função ou array... nesse caso o json...
+  results.forEach((item) => {
+    product.sku = item.id;
+    product.name = item.title;
+    product.image = item.thumbnail;
+    const section = createProductItemElement(product);
+    document.getElementsByClassName('items')[0].appendChild(section);
+  });
+};
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then(data => searchElement(data.results))
+    .then(response => response.json()).then(data => returnProduct(data.results))
     .catch(console.error);
 };
