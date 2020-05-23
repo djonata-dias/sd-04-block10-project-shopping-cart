406c30818039aab22831cd7b13053e2d7bd7bf73
cacobribeiro <cacobribeiro@gmail.com>
2020-05-23 15:43:23 -0300

    CC ok

--- script.js
+++ script.js
@@ -1,64 +1,4 @@
-
-// função para criar imagem do produto.
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-// Função que Cria os Buttons e addo listener com a função para jogar no carrinho.
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  if (element === 'button') {
-    moveToCart(e)
-  }
-  return e;
-}
-
-// função para criar lista de produtos retornados da API.
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  return section;
-}
-
-// Pega o ID da section que o addlistener está!
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-// função para acessa a section
-const moveToCart = e => {
-  e.addEventListener('click', function () {
-    return getSkuFromProductItem(e.parentNode)
-  })
-}
-
-// function cartItemClickListener(event) {
-// }
-
-
-// Função que cria o elemento no Carrinho de compras.
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-// }
-// feitos
-
 const query = 'computador';
-
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
 const pegaInfos = ({ id, title, salePrice, thumbnail }, callback) => {
@@ -70,6 +10,73 @@ const pegaInfos = ({ id, title, salePrice, thumbnail }, callback) => {
   };
   return callback(obj);
 };
+
+// função para criar imagem do produto.
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+const request2 = async (id) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const productJson = await response.json();
+    console.log(productJson);
+  } catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+};
+
+// Pega o ID da section que o addlistener está!
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// Função que cria o elemento no Carrinho de compras.
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', console.log('rodou!'));
+  return li;
+}
+
+// função para acessa a section
+const moveToCart = (e) => {
+  const ol = document.querySelector('ol.cart__items');
+  e.addEventListener('click', function () {
+    const id = getSkuFromProductItem(e.parentNode);
+    const json = request2(id);
+    return ol.appendChild(pegaInfos(json, createCartItemElement));
+  });
+};
+
+// Função que Cria os Buttons e addo listener com a função para jogar no carrinho.
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  if (element === 'button') {
+    moveToCart(e);
+  }
+  return e;
+}
+
+// função para criar lista de produtos retornados da API.
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
+
+// function cartItemClickListener(event) {
+// }
 
 const criaList = async () => {
   const listItens = document.querySelector('.items');
@@ -85,20 +92,6 @@ const criaList = async () => {
   }
 };
 
-
-const request2 = async (id) => {
-  try {
-    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
-    const productJson = await response.json();
-    console.log(productJson);
-  } catch (error) {
-    console.log(`ERROR: ${error}`)
-  }
-}
-
 window.onload = () => {
   criaList();
-
 };
-
-
