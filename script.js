79f919c8af871233edd0a41f0996aec8ff696ee2
cacobribeiro <cacobribeiro@gmail.com>
2020-05-23 14:47:49 -0300

    Implementada funçao de addlistener e pegar o ID do produto selecionado!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// função para criar imagem do produto.
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,13 +7,18 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Função que Cria os Buttons e addo listener com a função para jogar no carrinho.
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    moveToCart(e)
+  }
   return e;
 }
 
+// função para criar lista de produtos retornados da API.
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -20,43 +27,45 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// Pega o ID da section que o addlistener está!
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// função para acessa a section
+const moveToCart = e => {
+  e.addEventListener('click', function () {
+    return getSkuFromProductItem(e.parentNode)
+  })
+}
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
+
+// Função que cria o elemento no Carrinho de compras.
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 // }
 // feitos
-
-const limpaCarrinhoFunction = () => {
-  const limpaCarrinho = document.querySelector('.empty-cart');
-  limpaCarrinho.addEventListener('click', function () {
-    document.querySelector('.cart__items').innerHTML = '';
-    localStorage.clear();
-  });
-};
 
 const query = 'computador';
 
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
-const pegaInfos = ({ id, title, thumbnail }, callback) => {
+const pegaInfos = ({ id, title, salePrice, thumbnail }, callback) => {
   const obj = {
     sku: id,
     name: title,
+    price: salePrice,
     image: thumbnail,
   };
   return callback(obj);
@@ -76,8 +85,20 @@ const criaList = async () => {
   }
 };
 
+
+const request2 = async (id) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const productJson = await response.json();
+    console.log(productJson);
+  } catch (error) {
+    console.log(`ERROR: ${error}`)
+  }
+}
+
 window.onload = () => {
   criaList();
 
-  limpaCarrinhoFunction();
 };
+
+
