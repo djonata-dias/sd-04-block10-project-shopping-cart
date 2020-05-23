1e8874a881ceae05f5fbf96c6ec740f046e60387
Julio Cesar <stininmatic@hotmail.com>
2020-05-23 00:47:57 -0300

    Tentando o segundo requisito

--- script.js
+++ script.js
@@ -26,11 +26,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+//  console.log(sku, name, salePrice)
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 const convertObject = (dataArray) => {
   const itemClass = document.querySelector('.items');
-  dataArray.forEach(({ id, title, thumbnail }) => {
+  dataArray.forEach(({ id, title, thumbnail, price }) => {
     itemClass.appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail }));
     console.log(itemClass);
+    itemClass.lastElementChild.addEventListener('click', (event) => {
+      const choice = event.target;
+      console.log(choice);
+      const cartItems = document.querySelector('.cart__items');
+      cartItems.appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
+    });
   });
 };
 
@@ -42,20 +65,4 @@ const getObject = (busca) => {
   .then(data => convertObject(data.results));
 };
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-getObject('TV');
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+getObject('Computador');
