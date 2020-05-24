272bdb3db6629cd6c3fe587d8de963031071370a
Julio Cesar <stininmatic@hotmail.com>
2020-05-23 23:46:14 -0300

    Requisito esvaziar carrinho!

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() {};
+
+const buttonErase = document.querySelector('.empty-cart');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -30,16 +32,21 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 function cartItemClickListener(event) {
+  // ESTA FUNÇÃO RETIRA OS ITEMS DO CARRINHO UTILIZANDO O PARAMETRO EVENT
   // coloque seu código aqui
   const selecionado = event.target;
+  // O PARAMETRO EVENT ATRAVÉS DO TARGET MAPEIA O LOCAL CLICADO COM
+  // O MOUSE E É ATRIBUÍDO A VARIÁVEL SELECIONADO
   selecionado.innerHTML = '';
-  // selecionado.innerHTML = "";
-  // const carrinho = document.querySelector('.cart_items');
+  // A VARIÁVEL SELECIONADO É PREENCHIDA COM VALOR NENHUM PARA QUE POSSA SER APAGADA DO CARRINHO
   selecionado.parentNode.removeChild(selecionado);
+  // A VARIÁVEL SELECIONADO É UMA REFERÊNCIA PARA QUE POSSAMOS BUSCAR O PAI DO ELEMENTO
+  // HTML ATRAVÉS DO COMANDO PARENTNODE, A PARTIR DISTO, O ELEMENTO
+  // É REMOVIDO COM O COMANDO REMOVECHILD
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-//  console.log(sku, name, salePrice)
+  //  console.log(sku, name, salePrice)
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -48,27 +55,49 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 const convertObject = (dataArray) => {
+  // ESTA FUNÇÃO RETIRA OS CAMPOS DO OBJETO QUE SÃO NECESSÁRIOS
   const itemClass = document.querySelector('.items');
+  // PRIMEIRO SELECIONAMOS O ITEM HTML .items E O ATRBUÍMOS A VARIÁVEL itemClass
   dataArray.forEach(({ id, title, thumbnail, price }) => {
-    itemClass.appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail }));
-    console.log(itemClass);
-    itemClass.lastElementChild.addEventListener('click', (event) => {
-      const choice = event.target;
-      console.log(choice);
+    // UM LOOP É INICIADO PARA QUE OS ITEMS ID, TITLE E PRICE SEJAM COPIADOS
+    // DA LISTA DE TODOS OS ITEMS
+    itemClass.appendChild(
+      createProductItemElement({ sku: id, name: title, image: thumbnail })); 
+    // OS VALORES COPIADOS SÃO SETADOS NOS CAMPOS SKU, NAME E IMAGE, AUTOMATICAMENTE ESTES
+    // VALORES SÃO INSERIDOS NO ELEMENTO HTML ATRAVÉS DE ITEMCLASS
+    itemClass.lastElementChild.addEventListener('click', () => {
+      // ESSA FUNÇÃO INSERE OS ITEMS AO CARRINHO DE COMPRAS USANDO EVENT
       const cartItems = document.querySelector('.cart__items');
-      const itemCart = createCartItemElement({ sku: id, name: title, salePrice: price });
+      // ATRIBUÍMOS O ELEMENTO CART_ITEMS HTML A VARIÁVEL cartItems
+      const itemCart = createCartItemElement({
+        sku: id,
+        name: title,
+        salePrice: price,
+      }); // CRIAMOS O ELEMENTO HTML LI COM OS DADOS PASSADOS DO dataArray
+      itemCart.classList.add('added');
       cartItems.appendChild(itemCart);
-//      cartItems.addEventListener('click', cartItemClickListener)
+      // INSERIMOS O ELEMENTO LI COM SEUS VALORES COMO FILHO DA LISTA CART_ITEMS
+      // localStorage.setItem('carrinho', itemCart);
     });
   });
 };
 
+// FUNÇÃO QUE PEGA O OBJETO E FORMATA NO MODO JSON PARA QUE POSSAMOS MANIPULÁ-LO
 const getObject = (busca) => {
-  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q='; // USA O ENDEREÇO DA API DO MERCADO LIVRE
 
-  fetch(API_URL + busca)
-  .then(response => response.json())
-  .then(data => convertObject(data.results));
+  fetch(API_URL + busca) // FUNÇÃO FETCH USA O LINK PARA BUSCAR A PESQUISA DE COMPUTADOR
+    .then(response => response.json()) // TRANSFORMA O OBJETO PARA MANIPULAÇÃO
+    .then(data => convertObject(data.results));
+    // EXTRAI AS INFORMAÇÕES DE RESULTS, QUE É A QUE CONTEM OS CAMPOS QUE PRECISAMOS
 };
 
-getObject('Computador');
+getObject('Computador'); // ESSA CHAMADA INICIA O PROGRAMA CHAMANDO A FUNÇÃO
+// GETOBJECT E BUSCAR A PALAVRA 'COMPUTADOR'
+
+const eraseCart = () => {
+  const cartList = document.querySelector('.cart__items');
+  cartList.innerHTML = '';
+};
+
+buttonErase.addEventListener('click', eraseCart);
