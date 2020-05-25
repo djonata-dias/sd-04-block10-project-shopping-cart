c2562c7112eec9f2fa92b0813fd42ca2e39fd3ed
Ana Higo <anahigo.arq@gmail.com>
2020-05-25 19:30:52 -0300

    teroricamente seria o item 1, sera que passa?

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-window.onload = function onload() { };
-
-function createProductImageElement(imageSource) {
+const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+};
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// const cartItemClickListener = (event) {
+//   // coloque seu código aqui
+// };
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// const createCartItemElement = ({ sku, name, salePrice }) => {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// };
+
+
+const productInfo = (results) => {
+  const product = {};
+  results.forEach((item) => {
+    product.sku = item.id;
+    product.name = item.title;
+    product.image = item.thumbnail;
+    const section = createProductItemElement(product);
+    document.getElementsByClassName('items')[0].appendChild(section);
+  });
+};
+
+window.onload = function onload() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(response => response.json())
+    .then(data => productInfo(data.results))
+    .catch(console.error);
+};
