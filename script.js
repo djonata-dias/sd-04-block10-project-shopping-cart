879a3e02197b58f1d9bb9d10c1f7022cf754e9c0
ederusena <eder.sena@live.com>
2020-05-25 15:27:21 -0300

    projeto- fetch

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const updatePrice = async () => {
+  let totalPrice = 0;
+  try {
+    const itemsList = await document.querySelectorAll('ol.cart__items > li');
+    itemsList.forEach((itemElement) => {
+      const elementInnerHTMLarray = itemElement.innerHTML.split(' ');
+      const elementPriceArray = elementInnerHTMLarray[
+        elementInnerHTMLarray.length - 1
+      ].split('');
+      elementPriceArray.splice(0, 1);
+      const elementPriceNumber = Number(elementPriceArray.join(''));
+      totalPrice += elementPriceNumber;
+    });
+    document.querySelector('p.total-price').innerHTML = totalPrice;
+  } catch (err) {
+    console.log(err.message);
+  }
+  return totalPrice;
+};
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,11 +25,40 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  const itemsList = document.querySelector('ol.cart__items');
+  itemsList.removeChild(event.target);
+  localStorage.list = itemsList.innerHTML;
+  updatePrice();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  li.style.cursor = 'pointer';
+  return li;
+}
+
+function addToCartObj(obj) {
+  const { id: sku, title: name, price: salePrice } = obj;
+  return { sku, name, salePrice };
+}
+
 function addToCart(id) {
   fetch(`https://api.mercadolibre.com/items/${id}`)
     .then(response => response.json())
     .then(data => data)
-    .then(addToCartObj);
+    .then(addToCartObj)
+    .then(createCartItemElement)
+    .then((item) => {
+      const itemsList = document.querySelector('ol.cart__items');
+      itemsList.appendChild(item);
+      updatePrice();
+      return itemsList;
+    })
+    .then(list => (localStorage.list = list.innerHTML));
 }
 
 function createCustomElement(element, className, innerText, id = null) {
@@ -40,25 +89,16 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function listItems(array) {
+  const sectionContainer = document.querySelector('section.container');
+  const loadingMessage = document.querySelector('p.loading');
+  setTimeout(() => {
+    sectionContainer.removeChild(loadingMessage);
+  }, 500);
   const sectionItems = document.querySelector('section.items');
   array.forEach((computerObj) => {
     const { id: sku, title: name, thumbnail: image } = computerObj;
     const newObj = { sku, name, image };
-    // Adicione o elemento retornado da função createProductItemElement(product)
-    // como filho do elemento <section class="items">.
     sectionItems.appendChild(createProductItemElement(newObj));
   });
 }
@@ -68,20 +108,25 @@ const fetchList = () => {
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
-  // Você deve criar uma listagem de produtos que devem ser consultados através da API do Mercado Livre.
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', myObj)
     .then(response => response.json())
-    .then(data => data.results) //A lista de produtos que devem ser exibidos é o array results no JSON acima.
-    .then(listItems); // Você deve utilizar a função createProductItemElement(product) para criar os componentes HTML referentes a um produto.
-
+    .then(data => data.results)
+    .then(listItems);
 };
 
-function addToCartObj(obj) {
-  const { id: sku, title: name, price: salePrice } = obj;
-  console.log({ sku, name, salePrice });
-  return { sku, name, salePrice };
-}
-
 window.onload = function onload() {
+  if (typeof Storage !== 'undefined') {
+    document.querySelector('ol.cart__items').innerHTML = localStorage.list;
+    updatePrice();
+  }
+  document.querySelector('.empty-cart').addEventListener('click', () => {
+    localStorage.list = '';
+    const itemsList = document.querySelector('ol.cart__items');
+    while (itemsList.firstChild !== null) {
+      itemsList.removeChild(itemsList.firstChild);
+    }
+    updatePrice();
+  });
   fetchList();
+  updatePrice();
 };
