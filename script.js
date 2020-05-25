cb670e0ec0033ae67c7b0d59f65480fc9ebf9fb8
danilossousa <danilosantossousa@gmail.com>
2020-05-25 00:19:22 -0300

    v0.3.0.4 r

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-  const parent = event.target.parentNode;
-  parent.removeChild(event.target);
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
