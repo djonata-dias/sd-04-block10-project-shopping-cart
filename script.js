9de9c33e0ee1de5924e4511090ba570b462a9575
Pedro Pires <pedrotpoliveira@gmail.com>
2020-05-25 23:21:50 -0300

    2nd Req-Attempt 01

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const API_URL = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
-
+// GLOBALS
 const myObject = {
   method: "GET",
   headers: { Accept: "application/json" }
 };
 
+/* PRODUCTS ELEMENTS FUNCTIONS
+  - createProductImageElement(imageSource)
+  - createCustomElement(element, className, innerText)
+  - createProductItemElement({ sku, name, image })
+  - appendItems(data, items)
+  - fetchAllItems(query, items) */
+
 function createProductImageElement(imageSource) {
+  /* HELPER FUNCTION - Main Function: createProductItemElement()
+    Generates img element from API's item's thumbnail
+    Arguments:
+      - imageSource (URL): source of thumbnail (e.g.:"http://www.google.com/image.jpeg")
+    Returns:
+      - img (HTMLElement) */
   const img = document.createElement("img");
   img.className = "item__image";
   img.src = imageSource;
@@ -13,6 +25,14 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
+  /* HELPER FUNCTION - Main Function: createProductItemElement()
+    Generates HTML element according to given arguments
+    Arguments:
+      - element (string): type of element (e.g.: "img", "span", etc)
+      - className (string): append class name to element
+      - innerText (string): tag's content e.g.: "<span>innerText</span>"
+    Returns:
+      - e (HTMLElement): full HTMLElement to be appended */
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
@@ -20,6 +40,14 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
+  /* HELPER FUNCTION - Main Function: appendItems()
+    Generates HTML element according to given arguments
+    Arguments:
+      - sku (string): id value from ML API product data object
+      - name (string): name value from ML API product data object
+      - image (string): image value ML API product data object
+    Returns:
+      - section (HTMLElement): full HTMLElement to be appended */
   const section = document.createElement("section");
   section.className = "item";
 
@@ -32,13 +60,65 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-/*
+
+function appendItems(data, items) {
+  /* HELPER FUNCTION - Main Function: fetchAllItems()
+    Appends products from ML API Products Object
+    Arguments:
+      - data (Object): ML API Products Object
+      - items (HTMLCollection): HTMLCollection of all items with .items class
+    Returns:
+      Nothing */
+  data.results.forEach(result => {
+    const itemData = createProductItemElement({
+      sku: result.id,
+      name: result.title,
+      image: result.thumbnail
+    });
+    items[0].appendChild(itemData);
+  });
+  return document.querySelectorAll(".item__add");
+}
+
+async function fetchAllItems(query, items) {
+  /* Fetch data from ML API and append to index.html
+    Arguments:
+      - query (string): search query for ML API
+      - items (HTMLCollection): HTMLCollection of all items with .items class
+    Returns:
+      Nothing */
+  try {
+    const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    // prettier-ignore
+    const response = await fetch(API_URL, myObject);
+    const data = await response.json();
+    const btns = await appendItems(data, items);
+    return btns;
+  } catch (e) {
+    console.error(e);
+  }
+  return true;
+}
+
+/* CART ELEMENTS FUNCTIONS
+  - getSkuFromProductItem(item)
+  - cartItemClickListener(event)
+  - createCartItemElement({ sku, name, salePrice })
+  - appendCart(data)
+  - fetchCartItem(itemID) */
+
 function getSkuFromProductItem(item) {
+  /* HELPER FUNCTION - Main Function: fetchAllItems()
+    Retrives Item's SKU from parent <span>
+    Arguments:
+      - item (HTMLElement)
+    Returns:
+      - SKU Number (String) */
   return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  console.log(event);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -47,19 +127,44 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener("click", cartItemClickListener);
   return li;
-} */
+}
 
-window.onload = function onload() {
-  const sectionItems = document.querySelectorAll(".items");
+function fetchCartItem(itemID, cartID) {
+  const API_URL = `https://api.mercadolibre.com/items/${itemID}`;
   // prettier-ignore
   fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => data.results.forEach((result) => {
-      const itemData = createProductItemElement({
-        sku: result.id,
-        name: result.title,
-        image: result.thumbnail,
-      });
-      sectionItems[0].appendChild(itemData);
-    }));
+    .then((response) => response.json())
+    .then((productData) => {
+      cartID.appendChild(
+        createCartItemElement({
+          sku: productData.id,
+          name: productData.title,
+          salePrice: productData.price
+        })
+      );
+    })
+    .catch(error => console.log(error.message));
+}
+
+function appendCart(cart, btns) {
+  btns.forEach(e => {
+    const idThis = getSkuFromProductItem(e.parentNode);
+    e.addEventListener("click", () => {
+      fetchCartItem(idThis, cart);
+    });
+  });
+}
+
+window.onload = function onload() {
+  async function loadItems() {
+    try {
+      const sectionItems = await document.querySelectorAll(".items");
+      const newCart = await document.querySelector(".cart__items");
+      const btns = await fetchAllItems("computador", sectionItems);
+      await appendCart(newCart, btns);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  loadItems();
 };
