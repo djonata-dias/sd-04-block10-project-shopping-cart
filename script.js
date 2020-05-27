cba8c1ec9c170163e1ffef84067d5001c3c303f8
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-27 19:14:33 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 
-/*
-1. Listagem de produtos
-Você deve criar uma listagem de produtos que devem
-ser consultados através da API do Mercado Livre.
-Você deve utilizar o endpoint:
-"https://api.mercadolibre.com/sites/MLB/search?q=$QUERY"
-
-onde $QUERY deve ser o valor da sua busca.
-Para este trabalho, a busca deve ser o termo computador.
-O retorno desse endpoint será algo no formato json.
-Por exemplo, se for pesquisado "computador": veja no Json no readme.
-
-A lista de produtos que devem ser exibidos é o array results no JSON acima.
-
-Você deve utilizar a função createProductItemElement(product)
-para criar os componentes HTML referentes a um produto.
-
-Adicione o elemento retornado da função createProductItemElement(product)
-como filho do elemento <section class="items">.
-
-Obs: as variáveis sku, no código fornecido,
-se referem aos campos id retornados pela API.
-*/
-
 function searchProduct(product) {
   return `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
 }
@@ -41,24 +17,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
 
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -67,20 +32,46 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const btnAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+
+  btnAdd.addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then(data => data.json())
+      .then(product =>
+        document.getElementsByClassName('cart__items')[0].appendChild(
+          createCartItemElement({
+            sku: product.id,
+            name: product.title,
+            salePrice: product.price,
+          }),
+        ));
+  });
+  section.appendChild(btnAdd);
+
+  return section;
+}
 
 window.onload = function onload() {
   fetch(searchProduct('computador'))
-    .then(data => data.json())
-    .then((json) => {
-      json.results.forEach((product) => {
-        document.querySelector('.items').appendChild(
-          createProductItemElement({
-            sku: product.id,
-            name: product.title,
-            image: product.thumbnail,
-          }),
-        );
-      });
+  .then(data => data.json())
+  .then((json) => {
+    json.results.forEach((product) => {
+      document.querySelector('.items').appendChild(
+        createProductItemElement({
+          sku: product.id,
+          name: product.title,
+          image: product.thumbnail,
+        }),
+      );
     });
+  });
 };
