38d3ea4f9e411f4a1b8555dce3090e590ffd23e0
Pedro Pires <pedrotpoliveira@gmail.com>
2020-05-27 12:51:39 -0300

    Final Version with Docstrings

--- script.js
+++ script.js
@@ -23,11 +23,10 @@ let shoppingCart = getLSCart();
   - createCustomElement(element, className, innerText)
   - createProductItemElement({ sku, name, image })
   - appendItems(data, items)
-  - fetchAllItems(query, items) */
+  - loadPageItems(query, items) */
 
 function loadingStatus(x) {
-  /* HELPER FUNCTION - Main Function: loadPageItems()
-    Inserts or removes "LOADING" message on page
+  /* Inserts or removes "LOADING" message on page
     Arguments:
       - x (String): "add" or "del"
     Returns:
@@ -44,8 +43,7 @@ function loadingStatus(x) {
 }
 
 function createProductImageElement(imageSource) {
-  /* HELPER FUNCTION - Main Function: createProductItemElement()
-    Generates img element from API's item's thumbnail
+  /* Generates img element from API's item's thumbnail
     Arguments:
       - imageSource (URL): source of thumbnail (e.g.:"http://www.google.com/image.jpeg")
     Returns:
@@ -57,8 +55,7 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
-  /* HELPER FUNCTION - Main Function: createProductItemElement()
-    Generates HTML element according to given arguments
+  /* Generates HTML element according to given arguments
     Arguments:
       - element (string): type of element (e.g.: "img", "span", etc)
       - className (string): append class name to element
@@ -72,8 +69,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  /* HELPER FUNCTION - Main Function: appendItems()
-    Generates HTML element according to given arguments
+  /* Generates HTML element according to given arguments
     Arguments:
       - sku (string): id value from ML API product data object
       - name (string): name value from ML API product data object
@@ -94,13 +90,12 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 function appendItems(data, items) {
-  /* HELPER FUNCTION - Main Function: fetchAllItems()
-    Appends products from ML API Products Object
+  /* Appends products from ML API Products Object
     Arguments:
       - data (Object): ML API Products Object
       - items (HTMLCollection): HTMLCollection of all items with .items class
     Returns:
-      Nothing */
+      - Nothing */
   data.results.forEach(result => {
     const itemData = createProductItemElement({
       sku: result.id,
@@ -118,7 +113,7 @@ async function loadPageItems(query, items) {
       - query (string): search query for ML API
       - items (HTMLCollection): HTMLCollection of all items with .items class
     Returns:
-      Nothing */
+      - Nothing */
   try {
     const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
     await loadingStatus("add");
@@ -134,12 +129,24 @@ async function loadPageItems(query, items) {
 }
 
 /* CART ELEMENTS FUNCTIONS
-- getSkuFromProductItem(item)
-- cartItemClickListener(event)
-- createCartItemElement({ sku, name, salePrice }
-  - fetchCartItem(itemID) */
+  - updateTotal()
+  - addToLSCart(data)
+  - emptyCart()
+  - setClearBtn()
+  - getElementSku(item)
+  - cartItemClickListener(event)
+  - createCartItemElement({ sku, name, salePrice }
+  - appendCartToPage(data, type)
+  - addItemToCart(itemID)
+  - setAddToCartBtn(btns)
+  - loadCart() */
 
 function updateTotal() {
+  /* Sum cart total from shoppingCart Array and append info to HTML
+    Arguments:
+      - None
+    Returns:
+      - Nothing */
   let total = 0;
   if (shoppingCart.length > 0) {
     total = shoppingCart.reduce((acc, cur) => acc + cur.salePrice, 0);
@@ -148,36 +155,57 @@ function updateTotal() {
 }
 
 function addToLSCart(data) {
+  /* Updates cart data in localStorage
+    Arguments:
+      - data (object): item to append do cart array
+    Returns:
+      - Nothing */
   if (!data) {
     shoppingCart = [];
   } else {
     shoppingCart.push(data);
   }
   localStorage.setItem("CART_CONTENTS", JSON.stringify(shoppingCart));
+  // Updating Cart Total
   updateTotal();
 }
 
 function emptyCart() {
+  /* Empties cart
+    Arguments:
+      - None
+    Returns:
+      - Nothing */
   const olAll = document.querySelector(".cart__items");
   olAll.innerHTML = "";
+  // Update localStorage data with empty array
   addToLSCart();
 }
 
 function setClearBtn() {
+  /* Sets event listener on <button class="empty-cart">
+    Arguments:
+      - None
+    Returns:
+      - Nothing */
   btnEmpty[0].addEventListener("click", emptyCart);
 }
 
 function getElementSku(item) {
-  /* HELPER FUNCTION - Main Function: fetchAllItems()
-    Retrives Item's SKU from parent <span>
+  /* Retrives Item's SKU from parent <span>
     Arguments:
-      - item (HTMLElement)
+      - item (HTMLElement): <section class="item"> from main page
     Returns:
-      - SKU Number (String) */
+      - SKU Number (Number) */
   return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
+  /* Removes current item from cart
+    Arguments:
+      - event (Mouseevent)
+    Returns:
+      - Nothing */
   const itemSku = event.target.querySelector(".cart__sku").innerText;
   const itemIndex = shoppingCart.findIndex(e => e.sku === itemSku);
   shoppingCart.splice(itemIndex, 1);
@@ -187,6 +215,11 @@ function cartItemClickListener(event) {
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
+  /* Creates <li> with item info
+    Arguments:
+      - { sku, name, salePrice } (Object): Parsed item info from ML API
+    Returns:
+      - <li> with item info (HTMLElement) */
   const li = document.createElement("li");
   li.className = "cart__item";
   li.innerHTML = `SKU: <span class="cart__sku">${sku}</span> | NAME: ${name} | PRICE: $${salePrice}`;
@@ -196,6 +229,12 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 function appendCartToPage(data, type) {
+  /* Appends item data to <ol class="cart__items">
+  Arguments:
+    - data (Object): item to be appended
+    - type (String): specifies if data is already parsed
+  Returns:
+    - formatted item (Object) */
   const formatData = {
     sku: data.id,
     name: data.title,
@@ -211,6 +250,11 @@ function appendCartToPage(data, type) {
 }
 
 async function addItemToCart(itemID) {
+  /* Async function to handle API request and item insertion
+  Arguments:
+    - itemID (String): item to be appended
+  Returns:
+    - Nothing */
   try {
     const API_URL = `https://api.mercadolibre.com/items/${itemID}`;
     const response = await fetch(API_URL);
@@ -223,6 +267,11 @@ async function addItemToCart(itemID) {
 }
 
 function setAddToCartBtn(btns) {
+  /* Insert event handlers on main page buttons
+  Arguments:
+    - btns (NodeList): buttons
+  Returns:
+    - Nothing */
   btns.forEach(e => {
     const idThis = getElementSku(e.parentNode);
     e.addEventListener("click", () => {
@@ -232,6 +281,11 @@ function setAddToCartBtn(btns) {
 }
 
 function loadCart() {
+  /* Load cart from localStorage to HTML
+  Arguments:
+    - None
+  Returns:
+    - Nothing */
   shoppingCart.forEach(e => {
     appendCartToPage(e, "processed");
   });
@@ -239,10 +293,16 @@ function loadCart() {
   items.forEach(e => {
     e.addEventListener("click", cartItemClickListener);
   });
+  // Updating Cart Total
   updateTotal();
 }
 
 async function loadItems() {
+  /* Async handling of events on page
+  Arguments:
+    - None
+  Returns:
+    - Nothing */
   try {
     setClearBtn();
     await loadCart();
