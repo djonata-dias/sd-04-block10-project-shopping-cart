a262937638067536e0b1914d92774bee854dedb0
gabriellukke <gabriellukke@outlook.com>
2020-05-28 01:48:57 -0300

    Requirement 2 done

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const query = 'computador';
+const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+const myObject = {
+  method: 'GET',
+  headers: { Accept: 'application/json' },
+};
+
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -13,7 +20,6 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
-
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,6 +32,16 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: ${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+// Requirement 1
+// Create a product list
 const productList = (array) => {
   const items = document.querySelector('.items');
   array.forEach((product) => {
@@ -35,35 +51,53 @@ const productList = (array) => {
       name: title,
       image: thumbnail,
     };
-
     items.appendChild(createProductItemElement(item));
   });
 };
 
-/* const getSkuFromProductItem = item => item.querySelector('span.item__sku').innerText;
+// Requirement 2
+// Create an object and append to the Cart Items OL
+const addToCart = ({ id, title, price }) => {
+  const cartElemenentObj = {
+    sku: id,
+    name: title,
+    salePrice: price,
+  };
+  const cartItemElement = createCartItemElement(cartElemenentObj);
+  const cart = document.querySelector('.cart__items');
+  cart.appendChild(cartItemElement);
+};
 
-const cartItemClickListener = (event) => {
+// Fetch the API
+const fetchItem = (itemId) => {
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    .then(response => response.json())
+    .then(data => addToCart(data))
+    .catch(err => console.log(err));
+};
+
+// Get the item ID of clicked element
+const getItemId = (e) => {
+  const clickedItem = e.target.parentNode.firstChild.innerText;
+  fetchItem(clickedItem);
+};
+
+// Create Event Listeners for all items on the list and run the GetItemId function
+const clickedBtn = () => {
+  const addBtn = document.querySelectorAll('.item__add');
+  addBtn.forEach(button => button.addEventListener('click', getItemId));
+};
+
+// const getSkuFromProductItem = item => item.querySelector('span.item__sku').innerText;
+
+/* const cartItemClickListener = (event) => {
   // coloque seu código aqui
-}
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-} */
-
+}; */
 
 window.onload = () => {
-  const query = 'computador';
-  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  const myObject = {
-    method: 'GET',
-    headers: { Accept: 'application/json' },
-  };
   fetch(API_URL, myObject)
     .then(response => response.json())
     .then(data => productList(data.results))
+    .then(clickedBtn)
     .catch(() => console.log('error'));
 };
