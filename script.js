5f727f455343ece782c94d770d35d600fbe1e53e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-05-28 09:42:46 -0300

    codeclimate fixes applied

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const shopCart = document.querySelector('.cart__items');
 
-const addButton = () => {
-  const btn = document.querySelectorAll('.item__add');
-  btn.forEach(item => item.addEventListener('click', addToCart));
-};
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  event.target.remove();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 const addToCart = async (event) => {
   const targetButton = event.target;
@@ -16,6 +28,12 @@ const addToCart = async (event) => {
   });
   shopCart.appendChild(itemLi);
 };
+
+const addButton = () => {
+  const btn = document.querySelectorAll('.item__add');
+  btn.forEach(item => item.addEventListener('click', addToCart));
+};
+
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,22 +59,8 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  event.target.remove();
-}
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
 
 function createItemObject(array) {
   const productsSection = document.querySelector('.items');
@@ -75,7 +79,6 @@ const computerQuery = async () => {
   const jsonResult = await query.json();
   const queryResults = await jsonResult.results;
   await createItemObject(queryResults);
-  
 };
 
 window.onload = async () => {
