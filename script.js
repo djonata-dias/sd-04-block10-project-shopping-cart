6eeee5e48db25a5b7616ecd13f538e3dd7988965
saturnian99 <saturnian.dsg@gmail.com>
2020-05-29 18:56:51 -0300

    Created click listener for empty cart button, so it removes all items in the cart

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cart = document.getElementsByClassName('cart__items')[0];
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,12 +14,22 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener() {
+  cart.removeChild(this);
+}
+
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function addToCartClickListener(id) {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then(data => data.json())
+    .then(obj => cart.appendChild(createCartItemElement(obj)));
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -37,24 +49,21 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return item.querySelector('span.item__sku').innerText;
 } */
 
-const cart = document.getElementsByClassName('cart__items')[0];
-
-function cartItemClickListener() {
-  cart.removeChild(this)
+function removeAllClickListener() {
+  while (cart.firstChild) {
+    cart.removeChild(cart.lastChild);
+  }
 }
 
-function addToCartClickListener(id) {
-  fetch(`https://api.mercadolibre.com/items/${id}`)
-    .then(data => data.json())
-    .then(obj => cart.appendChild(createCartItemElement(obj)));
-}
+document.getElementsByClassName('empty-cart')[0]
+  .addEventListener('click', removeAllClickListener);
 
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then(data => data.json())
   .then(obj => obj.results)
   .then(arr => arr.forEach((product) => {
     document.getElementsByClassName('items')[0]
-      .appendChild(createProductItemElement(product));    
+      .appendChild(createProductItemElement(product));
   }));
 
 window.onload = function onload() { };
