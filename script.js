d7b6d15a64fea2a57ee5b6cb66d4011980d78d29
saturnian99 <saturnian.dsg@gmail.com>
2020-05-29 18:01:05 -0300

    Fixed CC script and index issues

--- script.js
+++ script.js
@@ -12,14 +12,22 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 // sku = id do produto
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
   section.addEventListener('click', () => {
     fetch(`https://api.mercadolibre.com/items/${sku}`)
-      .then((data) => data.json())
-      .then((obj) => document.getElementsByClassName('cart__items')[0]
+      .then(data => data.json())
+      .then(obj => document.getElementsByClassName('cart__items')[0]
         .appendChild(createCartItemElement(obj)));
   });
 
@@ -31,30 +39,20 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+} */
 
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-  .then((data) => data.json())
-  .then((obj) => obj.results)
-  .then((arr) => arr.forEach(product => {
+  .then(data => data.json())
+  .then(obj => obj.results)
+  .then(arr => arr.forEach((product) => {
     document.getElementsByClassName('items')[0]
-      .appendChild(
-        createProductItemElement(product)
-      )
+      .appendChild(createProductItemElement(product));
   }));
 
 window.onload = function onload() { };
