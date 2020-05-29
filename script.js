0a05ec8658efc939162d6400ffd3b661236b663d
saturnian99 <saturnian.dsg@gmail.com>
2020-05-29 18:35:27 -0300

    Implemented click listener to remove cart items individually

--- script.js
+++ script.js
@@ -20,16 +20,10 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
-// sku = id do produto
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-  section.addEventListener('click', () => {
-    fetch(`https://api.mercadolibre.com/items/${sku}`)
-      .then(data => data.json())
-      .then(obj => document.getElementsByClassName('cart__items')[0]
-        .appendChild(createCartItemElement(obj)));
-  });
+  section.addEventListener('click', () => addToCartClickListener(sku));
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
@@ -43,16 +37,24 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return item.querySelector('span.item__sku').innerText;
 } */
 
-/* function cartItemClickListener(event) {
-  // coloque seu código aqui
-} */
+const cart = document.getElementsByClassName('cart__items')[0];
+
+function cartItemClickListener() {
+  cart.removeChild(this)
+}
+
+function addToCartClickListener(id) {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then(data => data.json())
+    .then(obj => cart.appendChild(createCartItemElement(obj)));
+}
 
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then(data => data.json())
   .then(obj => obj.results)
   .then(arr => arr.forEach((product) => {
     document.getElementsByClassName('items')[0]
-      .appendChild(createProductItemElement(product));
+      .appendChild(createProductItemElement(product));    
   }));
 
 window.onload = function onload() { };
