cd84ddc1513469857194ece68863bd13bc3a70ae
André Ferreira <andrefpires1@gmail.com>
2020-06-11 00:22:40 -0300

    CC2 script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const myObject = { method: 'GET', headers: new Headers() };
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,47 +12,35 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-const sumPrices = () => {
+function setLoading(enable) {
+  if (enable) {
+    document.getElementsByClassName('items')[0]
+      .appendChild(createCustomElement('p', 'loading', 'Loading...'));
+  } else {
+    document.getElementsByClassName('loading')[0].remove();
+  }
+}
+
+const priceCart = () => {
   const cartItem = document.querySelectorAll('.cart__item');
-  document.getElementsByClassName('total-price')[0].textContent = Math.round([...cartItem].map(e => e.textContent
-    .match(/([0-9.]){1,}$/))
-    .reduce((acc, price) => acc + parseFloat(price), 0) * 100) / 100;
+  const cartPrice = document.querySelector('.total-price');
+  cartPrice.innerText = Math.round(
+    [...cartItem]
+    .map(item => item.textContent.match(/([0-9.]){1,}$/))
+    .reduce((acc, add) => acc + parseFloat(add), 0)
+    .toFixed(2) * 100) / 100;
 };
 
 const updateCart = () => {
-  localStorage.setItem('itemCart', document.getElementsByClassName('cart__items')[0].innerHTML);
-  sumPrices();
+  priceCart();
+  localStorage.setItem('Cart_items', document.getElementsByClassName('cart__items')[0].innerHTML);
+  localStorage.setItem('Total_price', document.getElementsByClassName('total-price')[0].innerHTML);
 };
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
 
 function cartItemClickListener(event) {
-  event.target.remove();
+  document.getElementsByClassName('cart__items')[0].removeChild(event.target);
   updateCart();
 }
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-const addElementToCart = async ({ sku }) => {
-  await fetch(`https://api.mercadolibre.com/items/${sku}`)
-  .then(data => data.json())
-  .then((product) => {
-    document.getElementsByClassName('cart__items')[0].appendChild(createCartItemElement({
-      sku: product.id,
-      name: product.title,
-      salePrice: product.price,
-    }));
-    updateCart();
-  });
-  await (localStorage.setItem('itemCart', document.getElementsByClassName('cart__items')[0].innerHTML));
-};
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
