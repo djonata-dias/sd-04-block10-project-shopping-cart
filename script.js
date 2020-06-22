ff57383771d7bfe2644157da81e2befd09eafb8e
Cezene <cezenesn@hotmail.com.br>
2020-06-22 03:57:39 -0300

    pfvr bot

--- script.js
+++ script.js
@@ -80,7 +80,6 @@ window.onload = async () => {
         },
         ));
     });
-    document.querySelector('.loading').remove();
   });
   document.getElementsByClassName('empty-cart')[0].addEventListener('click', async () => {
     document.getElementsByClassName('cart__items')[0].innerHTML = '';
@@ -89,4 +88,7 @@ window.onload = async () => {
   document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('Cart_items');
   document.querySelectorAll('li').forEach(li => li.addEventListener('click', cartItemClickListener));
   await updateCart();
+  setTimeout(() => {
+    document.querySelector('.loading').remove();
+  }, 3000);
 };
