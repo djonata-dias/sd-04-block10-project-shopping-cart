e246641217594cbcc660bf854b87980a5dd695fc
djonata-dias <diasdjonata@gmail.com>
2020-08-17 20:54:03 -0300

    implementei o botão de limpar carrinho

--- script.mjs
+++ script.mjs
@@ -1,17 +1,19 @@
 import requestAPI from './helpers/requestAPI.js';
 
-const api = async () => {
-  return (
-    document.getElementsByClassName('loading')[0].remove(),
-    await requestAPI('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
-  )
-};
+const loading = document.getElementsByClassName('loading')[0];
 const cart = document.getElementsByClassName('cart__items')[0];
 const totalPrice = document.getElementsByClassName('total-price')[0];
 const storageCart = JSON.parse(localStorage.getItem('shoppingCart'));
-const cartArray = storageCart ? storageCart : [];
+let cartArray = storageCart ? storageCart : [];
+const cleanButton = document.getElementsByClassName('empty-cart')[0];
+
+const api = async () => {
+  return (
+    await requestAPI('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+  )
+};
+
 const updateCart = (array) => {
-  if (!cartArray.length) localStorage.setItem('shoppingCart', JSON.stringify([]));
   localStorage.setItem('shoppingCart', JSON.stringify([...array]));
 }
 
@@ -46,15 +48,14 @@ function getSkuFromProductItem(item) {
 }
 
 async function cartItemClickListener(event, salePrice) {
-  totalPrice.innerText = +totalPrice.innerText - salePrice
-  console.log(event.target.innerHTML);
   cartArray.forEach((item, index) => {
     if (item === event.target.innerHTML) {
-      cartArray.splice(index, 1)
-    }
-  })
-  await event.target.remove()
-  updateCart(cartArray)
+      cartArray.splice(index, 1);
+    };
+  });
+  updateCart(cartArray);
+  await event.target.remove();
+  totalPrice.innerText = (+totalPrice.innerText - salePrice);
 }
 
 async function createCartItemElement(sku, name, salePrice) {
@@ -62,27 +63,31 @@ async function createCartItemElement(sku, name, salePrice) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', (e) => cartItemClickListener(e, salePrice));
-  cart.appendChild(li)
-  cartArray.push(li.innerHTML)
-  updateCart(cartArray)
-  totalPrice.innerText = +totalPrice.innerText + salePrice
+  cart.appendChild(li);
+  cartArray.push(li.innerHTML);
+  updateCart(cartArray);
+  totalPrice.innerText = (+totalPrice.innerText + salePrice);
 }
-const setInitialItens = async () => {
-  cartArray.forEach(item => {
+const setInitialItens = () => {
+  cartArray.forEach(async item => {
     const li = document.createElement('li');
-    const salePrice = +item.split('| PRICE: $')[1]
+    const salePrice = +item.split('| PRICE: $')[1];
     li.className = 'cart__item';
     li.innerText = item;
     li.addEventListener('click', (e) => cartItemClickListener(e, salePrice));
-    cart.appendChild(li)
-    totalPrice.innerText = +totalPrice.innerText + salePrice
+    await cart.appendChild(li);
+    totalPrice.innerText = (+totalPrice.innerText + salePrice);
   })
 }
-
+const cleanCart = () => {
+  cartArray = []
+  cart.innerHTML = cartArray;
+  updateCart([])
+}
+cleanButton.addEventListener('click', () => cleanCart());
 window.onload = async function onload() {
   const items = (await api()).results;
+  loading.remove();
+  setInitialItens();
   items.forEach((item) => createProductItemElement(item));
-  console.log(items);
-  updateCart(cartArray)
-  setInitialItens()
 };
